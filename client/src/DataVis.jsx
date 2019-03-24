db4039a755368ba7758e2310530f53138431599e
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2019-03-24 15:27:44 -0700

    made alterations to the dataVis.jsx, now the Radial Chart can display the correct color with the amount of the image

--- client/src/DataVis.jsx
+++ client/src/DataVis.jsx
@@ -5,45 +5,16 @@ import {RadialChart} from "react-vis";
 export default (props) => {
   const dataArr = props.cloudColors.map((color, index) => {
     console.log(color)
-    return{ angle: [color[1]], color: [color[0]], label: [color[0]] } 
+    return{ angle: [color[1]], color: [color[0]], label:[color[0], ["-"], color[1]]} 
   })
 
   return(
     <div className="colorList">
-    <RadialChart data={dataArr} width={300} height={300} showLabels={true} animation={true} />
+    <RadialChart data={dataArr} width={500} height={550} showLabels={true}  colorType="literal"/>
     </div>
   )
 }
 
-
-
-
-
-
-// const Chart = (props) => {
-
-//   const dataArr = props.data.map((d)=> {
-//       return {x: d.year + '/' + d.quarter, 
-//       y: parseFloat(d.count/1000)}
-//   });
-
-//   return (
-//       <XYPlot
-//           xType="ordinal"
-//           width={1000}
-//           height={500}>
-//           <VerticalGridLines />
-//           <HorizontalGridLines />
-//           <XAxis title="Period of time(year and quarter)" />
-//           <YAxis title="Number of pull requests (thousands)" />
-//               <LineSeries
-//                   data={dataArr}
-//                   style={{stroke: 'violet', strokeWidth: 3}}/>
-//       </XYPlot>
-//   );
-// }
-
-// export default Chart;
 
 
 
