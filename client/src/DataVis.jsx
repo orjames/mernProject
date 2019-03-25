f45a21a176b004caab48b7596ed9329aa842a937
orjames <orjames@berkeley.edu>
2019-03-25 10:45:36 -0700

    fixed datavis error

--- client/src/DataVis.jsx
+++ client/src/DataVis.jsx
@@ -5,11 +5,10 @@ export default (props) => {
   // To Do click functionality
   let dataArr = props.cloudColors.map((color, index) => {
     // console.log(color)
-<<<<<<< HEAD
     return {
-      angle: [color[1]],
+      angle: color[1],
       color: [color[0]],
-      label: [color[0], ['-'], color[1]],
+      label: `${color[0]} ${color[1]}`,
     };
   });
   return (
@@ -21,13 +20,6 @@ export default (props) => {
         showLabels={true}
         colorType='literal'
       />
-=======
-    return{ angle: [color[1]], color: [color[0]], label:[color[0], ["-"], color[1]]} 
-  })
-  return(
-    <div className="colorList">
-    <RadialChart data={dataArr} width={500} height={550} showLabels={true}  colorType="literal"/>
->>>>>>> 4fe0c633ca408a4ad310fa2d816d97f709696247
     </div>
   );
 };
