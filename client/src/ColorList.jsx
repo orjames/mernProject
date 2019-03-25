e7f5b48b80c41c73d81b3de21b3c195abeb64671
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2019-03-25 14:32:46 -0700

    got the API to render data that we would want on the page of ColorList.jsx

--- client/src/ColorList.jsx
+++ client/src/ColorList.jsx
@@ -6,20 +6,20 @@ console.log(props, "this is your props")
 let colors = props.colorRec.colors.map((color, index) => {
    return (
    <div className="colorRecsList" key="index">
-
-{/* 
-            <h1>
-            {color.hex.value}, {color.name.value}
-            </h1>
-            <img src={color.image.named} alt='' /> */}
+      <h3 classname="compleColorName">{color.name.value}</h3>
+      <h3 classname="compleColorHex">{color.hex.value}</h3>
+      <img src ={color.image.bare} width="30%" alt={color.name.value} /> 
    </div>
    );
 })
 
 return (
-   <h1> Color List </h1>
-)
-}
+   <div>
+   <h1> Your COMPLé's </h1>
+   {colors}
+   </div>
+)}
+
 
 export default ColorList; 
 
