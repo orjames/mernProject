8511351d2440f2716b773070956e22437ce4d36c
orjames <orjames@berkeley.edu>
2019-04-10 17:43:27 -0700

    heroku

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://react-image-upload.herokuapp.com'
+    ? 'https://orj-mern-project.herokuapp.com/'
     : 'http://localhost:3000';
