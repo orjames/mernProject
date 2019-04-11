bca2afab695b74f37e00573b2e631b1e296fb620
orjames <orjames@berkeley.edu>
2019-04-10 18:12:42 -0700

    heroku

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://orj-mern-project.herokuapp.com/'
+    ? 'https://orj-mern-project.herokuapp.com'
     : 'http://localhost:3000';
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.CLIENT_ORIGIN =
   process.env.NODE_ENV === 'production'
-    ? 'https://orj-mern-project.herokuapp.com/'
+    ? 'https://orj-mern-project.herokuapp.com'
     : 'http://localhost:3000';
